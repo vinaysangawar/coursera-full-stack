0c8f383ad59e3c7b7cf3b80a39aa9f353240f55c
Vinay Sangawar <vinaysangawar@gmail.com>
2016-08-18 22:12:34 -0400

    Assignment 2 - Angular forms with validation

--- Front-End Javascript Frameworks - AngularJS/conFusion/app/scripts/app.js
+++ Front-End Javascript Frameworks - AngularJS/conFusion/app/scripts/app.js
@@ -1,10 +1,13 @@
 'use strict';
+
 angular.module('confusionApp', [])
 
     .controller('MenuController', ['$scope', function ($scope) {
+
         $scope.tab = 1;
         $scope.filtText = '';
         $scope.showDetails = false;
+
         $scope.dishes = [
             {
                 name: 'Uthapizza',
@@ -46,6 +49,7 @@ angular.module('confusionApp', [])
 
         $scope.select = function (setTab) {
             $scope.tab = setTab;
+
             if (setTab === 2) {
                 $scope.filtText = "appetizer";
             }
@@ -70,30 +74,103 @@ angular.module('confusionApp', [])
     }])
 
     .controller('ContactController', ['$scope', function ($scope) {
+
         $scope.feedback = {mychannel: "", firstName: "", lastName: "", agree: false, email: ""};
+
         var channels = [{value: "tel", label: "Tel."}, {value: "Email", label: "Email"}];
+
         $scope.channels = channels;
         $scope.invalidChannelSelection = false;
+
     }])
 
     .controller('FeedbackController', ['$scope', function ($scope) {
+
         $scope.sendFeedback = function () {
+
             console.log($scope.feedback);
-            if ($scope.feedback.agree && ($scope.feedback.mychannel === "") && !$scope.feedback.mychannel) {
+
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
                 $scope.invalidChannelSelection = true;
                 console.log('incorrect');
             }
             else {
                 $scope.invalidChannelSelection = false;
-                $scope.feedback = {
-                    mychannel: "", firstName: "", lastName: "",
-                    agree: false, email: ""
-                };
+                $scope.feedback = {mychannel: "", firstName: "", lastName: "", agree: false, email: ""};
                 $scope.feedback.mychannel = "";
-
                 $scope.feedbackForm.$setPristine();
                 console.log($scope.feedback);
             }
         };
     }])
+
+    .controller('DishDetailController', ['$scope', function ($scope) {
+
+        var dish = {
+            name: 'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains',
+            label: 'Hot',
+            price: '4.99',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+
+            ]
+        };
+
+        $scope.dish = dish;
+
+    }])
+
+    .controller('DishCommentController', ['$scope', function ($scope) {
+
+        $scope.comment = {rating: 5, comment: "", author: "", date: ""};
+
+        $scope.submitComment = function () {
+
+            //Step 2: This is how you record the date
+            $scope.comment.date = new Date().toISOString();
+
+            // Step 3: Push your comment into the dish's comment array
+            $scope.dish.comments.push($scope.comment);
+            console.log($scope.comment);
+
+            //Step 4: reset your form to pristine
+            $scope.commentForm.$setPristine();
+
+            //Step 5: reset your JavaScript object that holds your comment
+            $scope.comment = {rating: 5, comment: "", author: "", date: ""};
+        }
+    }])
+
 ;
